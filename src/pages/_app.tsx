b044b40dceb258bff944dc724333a7b9cae4c309
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2023-10-22 14:29:00 +0000

    add fontawesome to root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
+import { config } from '@fortawesome/fontawesome-svg-core'
 import { ToastProvider } from "@/providers/ToastProvider"
-import "@/styles/globals.css"
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import type { AppProps } from "next/app"
+import "@/styles/globals.css"
+config.autoAddCss = false
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
